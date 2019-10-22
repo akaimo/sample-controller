85bfc72f9563216b80a0db0cca12f161f2c40bc0
akaimo <kawashu0723@gmail.com>
2019-10-22 16:53:49 +0900

    create controller

--- controller.go
+++ controller.go
@@ -2,19 +2,74 @@ package main
 
 import (
 	clientset "github.com/akaimo/sample-controller/pkg/client/clientset/versioned"
+	samplescheme "github.com/akaimo/sample-controller/pkg/client/clientset/versioned/scheme"
 	informers "github.com/akaimo/sample-controller/pkg/client/informers/externalversions/samplecontroller/v1"
+	listers "github.com/akaimo/sample-controller/pkg/client/listers/samplecontroller/v1"
+	corev1 "k8s.io/api/core/v1"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	jobinformers "k8s.io/client-go/informers/batch/v1"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes/scheme"
+	typedcorev1 "k8s.io/client-go/kubernetes/typed/core/v1"
+	batchlisters "k8s.io/client-go/listers/batch/v1"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/tools/record"
+	"k8s.io/client-go/util/workqueue"
+	"k8s.io/klog"
 )
 
-type Controller struct{}
+const controllerAgentName = "sample-controller"
+
+type Controller struct {
+	// kubeclientset is a standard kubernetes clientset
+	kubeclientset kubernetes.Interface
+	// sampleclientset is a clientset for our own API group
+	sampleclientset clientset.Interface
+
+	jobLister            batchlisters.JobLister
+	jobSynced            cache.InformerSynced
+	sampleResourceLister listers.SampleResourceLister
+	sampleResourceSynced cache.InformerSynced
+
+	// workqueue is a rate limited work queue. This is used to queue work to be
+	// processed instead of performing it as soon as a change happens. This
+	// means we can ensure we only process a fixed amount of resources at a
+	// time, and makes it easy to ensure we are never processing the same item
+	// simultaneously in two different workers.
+	workqueue workqueue.RateLimitingInterface
+	// recorder is an event recorder for recording Event resources to the
+	// Kubernetes API.
+	recorder record.EventRecorder
+}
 
 func NewController(
 	kubeclientset kubernetes.Interface,
 	sampleclientset clientset.Interface,
 	jobInformer jobinformers.JobInformer,
 	sampleinformer informers.SampleResourceInformer) *Controller {
-	return &Controller{}
+
+	// Create event broadcaster
+	// Add sample-controller types to the default Kubernetes Scheme so Events can be
+	// logged for sample-controller types.
+	utilruntime.Must(samplescheme.AddToScheme(scheme.Scheme))
+	klog.V(4).Info("Creating event broadcaster")
+	eventBroadcaster := record.NewBroadcaster()
+	eventBroadcaster.StartLogging(klog.Infof)
+	eventBroadcaster.StartRecordingToSink(&typedcorev1.EventSinkImpl{Interface: kubeclientset.CoreV1().Events("")})
+	recorder := eventBroadcaster.NewRecorder(scheme.Scheme, corev1.EventSource{Component: controllerAgentName})
+
+	controller := &Controller{
+		kubeclientset:        kubeclientset,
+		sampleclientset:      sampleclientset,
+		jobLister:            jobInformer.Lister(),
+		jobSynced:            jobInformer.Informer().HasSynced,
+		sampleResourceLister: sampleinformer.Lister(),
+		sampleResourceSynced: sampleinformer.Informer().HasSynced,
+		workqueue:            workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), "Samples"),
+		recorder:             recorder,
+	}
+
+	return controller
 }
 
 // Run will set up the event handlers for types we are interested in, as well
